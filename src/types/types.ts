d28a66d52e9a183d015a7f4061532478a57bf093
Karim <chammout.karim@gmail.com>
2024-12-10 15:29:48 +0200

    Extend Express `Request` type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,16 @@
+import { JwtPayload } from 'jsonwebtoken';
 import { Knex } from 'knex';
 
 import { ReadersType } from '../graphql/ctx/db';
+
+declare global {
+  namespace Express {
+    interface Request {
+      tokenPayload: JwtPayload | null;
+      token: string | null;
+    }
+  }
+}
 
 export type ContextType = {
   db: Knex;
