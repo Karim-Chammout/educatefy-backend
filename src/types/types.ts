669f86c32c904c9f85dfa4e8e4bdcfe312171ce7
Karim <chammout.karim@gmail.com>
2024-12-10 21:47:41 +0200

    Add `UserContext` type

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,6 +2,7 @@ import { JwtPayload } from 'jsonwebtoken';
 import { Knex } from 'knex';
 
 import { ReadersType } from '../graphql/ctx/db';
+import { FsContext } from '../graphql/ctx/fs';
 
 declare global {
   namespace Express {
@@ -12,7 +13,34 @@ declare global {
   }
 }
 
+type BaseUserContext = {
+  ip: string;
+  userAgent: string;
+};
+
+export type AuthenticatedUserContext = BaseUserContext & {
+  id: number;
+  authenticated: true;
+  roleId: number;
+  countryId?: number | null;
+};
+
+export type PublicUserContext = BaseUserContext & {
+  authenticated: false;
+};
+
+export type UserContext = AuthenticatedUserContext | PublicUserContext;
+
 export type ContextType = {
+  user: UserContext;
   db: Knex;
   loaders: ReadersType;
+  fs: FsContext;
+};
+
+export type AuthenticatedCtxType = {
+  user: AuthenticatedUserContext;
+  db: Knex;
+  loaders: ReadersType;
+  fs: FsContext;
 };
