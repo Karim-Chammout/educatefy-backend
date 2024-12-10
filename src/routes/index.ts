bc094c221943635a214635c21576cc09ba647211
Karim <chammout.karim@gmail.com>
2024-12-10 23:46:04 +0200

    Create `/file` router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
 import authRouter from './auth';
+import fileRouter from './file';
 
 const api = express.Router();
 
 api.use('/openid', authRouter);
+
+api.use('/file', fileRouter);
 
 export default api;
