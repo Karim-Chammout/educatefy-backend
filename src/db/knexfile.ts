d2aad90311b7ed8244ba89dd866aae39a32c7c98
Karim <chammout.karim@gmail.com>
2024-12-10 07:32:31 +0200

    Install and configure knex

---
+++ src/db/knexfile.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import { Knex } from 'knex';
+
+dotenv.config({ path: '../../.env' });
+
+const knexConfig: Knex.Config = {
+  client: 'pg',
+  connection: {
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT),
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'migrations',
+    directory: './migrations',
+  },
+};
+
+export default knexConfig;
