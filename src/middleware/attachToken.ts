e356ba4edcccfe80f8708e5010b1a985720445fd
Karim <chammout.karim@gmail.com>
2024-12-10 15:28:58 +0200

    Create `attachToken` middleware

---
+++ src/middleware/attachToken.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { verifyJWT } from '../utils/jwt';
+
+export const tokenToRequest = async (token: string, req: Request) => {
+  if (token) {
+    try {
+      req.tokenPayload = await verifyJWT(token);
+      req.token = token;
+    } catch (e) {
+      req.tokenPayload = null;
+      req.token = null;
+    }
+  }
+};
+
+export async function attachToken(req: Request, res: Response, next: NextFunction) {
+  // Initialize the properties
+  if (!req.tokenPayload && !req.token) {
+    req.tokenPayload = null;
+    req.token = null;
+  }
+
+  let token: string | null = null;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    token = req.headers.authorization.substring(7);
+  }
+
+  if (!token) {
+    return next();
+  }
+
+  await tokenToRequest(token, req);
+
+  return next();
+}
