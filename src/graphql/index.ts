0669f1e2900d7df22c2b806de6c54cf3aad12df6
Karim <chammout.karim@gmail.com>
2024-12-10 09:41:10 +0200

    Pass the `db` & `loaders` to the GQL context

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import { createYoga } from 'graphql-yoga';
 
+import { db } from '../db';
+import { createLoaders } from './ctx/db';
 import Schema from './schema/Schema';
 
 export const yoga = createYoga({
   schema: Schema,
   landingPage: false,
   graphqlEndpoint: '/',
+  context: async (ctx) => {
+    return {
+      db,
+      loaders: { ...createLoaders(db) },
+    };
+  },
 });
 
 export default function GraphQL() {
