b5d05df253ad040231625bd84511d8b265cfe8e5
Karim <chammout.karim@gmail.com>
2024-12-10 09:41:32 +0200

    Create `OpenIdClient` GQL type

---
+++ src/graphql/schema/types/OpenIdClient.ts
@@ -0,0 +1,30 @@
+import { GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+
+import { ContextType } from '../../../types/types';
+
+export const OpenIdClient = new GraphQLObjectType<any, ContextType>({
+  name: 'OpenIdClient',
+  description: 'The open id client providers to use for auth',
+  fields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+      description: 'A unique id of this OIDC',
+    },
+    identity_provider: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The openId provider',
+    },
+    button_text: {
+      type: GraphQLString,
+      description: 'The text of the OIDC that should be displayed on the button',
+    },
+    button_icon: {
+      type: GraphQLString,
+      description: 'The icon of the OIDC that should be displayed on the button',
+    },
+    button_background_color: {
+      type: GraphQLString,
+      description: 'The button background color of the OIDC',
+    },
+  },
+});
