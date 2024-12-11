45d654839c1c84e256a4380f1e8bfe7b6c9a537a
Karim <chammout.karim@gmail.com>
2024-12-11 21:17:01 +0200

    Create `AccountInfo` input type

---
+++ src/graphql/schema/inputs/AccountInfo.ts
@@ -0,0 +1,53 @@
+import { GraphQLID, GraphQLInputObjectType, GraphQLNonNull, GraphQLString } from 'graphql';
+
+import GraphQLDate from '../Scalars/Date';
+import Gender from '../types/enum/Gender';
+
+const AccountInfoInput = new GraphQLInputObjectType({
+  name: 'AccountInfoInput',
+  description: 'Input for updating an account information',
+  fields: {
+    firstName: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The first name of the user.',
+    },
+    lastName: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The last name of the user.',
+    },
+    nickname: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The nickname name of the user.',
+    },
+    nationalityId: {
+      type: new GraphQLNonNull(GraphQLID),
+      description: 'The nationality of the user',
+    },
+    countryId: {
+      type: new GraphQLNonNull(GraphQLID),
+      description: 'The current country of the user',
+    },
+    gender: {
+      type: new GraphQLNonNull(Gender),
+      description: 'The gender of the user.',
+    },
+    dateOfBirth: {
+      type: new GraphQLNonNull(GraphQLDate),
+      description: 'The date of birth of the user.',
+    },
+    teacherSpecialty: {
+      type: GraphQLString,
+      description: 'The specialty of the teacher.',
+    },
+    teacherBio: {
+      type: GraphQLString,
+      description: 'The short bio about the teacher.',
+    },
+    teacherDescription: {
+      type: GraphQLString,
+      description: 'The short description about the teacher.',
+    },
+  },
+});
+
+export default AccountInfoInput;
