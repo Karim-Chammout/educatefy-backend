c2e31219984d874a763f423d3f641abc0ff44f11
Karim <chammout.karim@gmail.com>
2024-12-11 21:20:33 +0200

    Generate GQL types

--- src/types/schema-types.ts
+++ src/types/schema-types.ts
@@ -12,12 +12,124 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean; }
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
+  /** Date custom scalar type */
+  Date: { input: any; output: any; }
 };
+
+/** The properties of the account */
+export type Account = {
+  __typename?: 'Account';
+  /** The role of the account. */
+  accountRole: AccountRole;
+  /** The avatar url of this account (provided by the openid-provider) */
+  avatar_url?: Maybe<Scalars['String']['output']>;
+  /** The bio of the teacher. */
+  bio?: Maybe<Scalars['String']['output']>;
+  /** The current country the user stated to live in. */
+  country?: Maybe<Country>;
+  /** The date of birth of this account */
+  date_of_birth?: Maybe<Scalars['Date']['output']>;
+  /** A detailed overview about this teacher. */
+  description?: Maybe<Scalars['String']['output']>;
+  /** The first name of the account */
+  first_name?: Maybe<Scalars['String']['output']>;
+  /** The gender of the account */
+  gender?: Maybe<Gender>;
+  /** A unique id of this account */
+  id: Scalars['ID']['output'];
+  /** The last name of the account */
+  last_name?: Maybe<Scalars['String']['output']>;
+  /** The name of the account */
+  name?: Maybe<Scalars['String']['output']>;
+  /** The nationality of the user */
+  nationality?: Maybe<Country>;
+  /** The nickname of the account */
+  nickname?: Maybe<Scalars['String']['output']>;
+  /** Represents the subject a teacher is specialized in for teaching. */
+  specialty?: Maybe<Scalars['String']['output']>;
+};
+
+/** Input for updating an account information */
+export type AccountInfoInput = {
+  /** The current country of the user */
+  countryId: Scalars['ID']['input'];
+  /** The date of birth of the user. */
+  dateOfBirth: Scalars['Date']['input'];
+  /** The first name of the user. */
+  firstName: Scalars['String']['input'];
+  /** The gender of the user. */
+  gender: Gender;
+  /** The last name of the user. */
+  lastName: Scalars['String']['input'];
+  /** The nationality of the user */
+  nationalityId: Scalars['ID']['input'];
+  /** The nickname name of the user. */
+  nickname: Scalars['String']['input'];
+  /** The short bio about the teacher. */
+  teacherBio?: InputMaybe<Scalars['String']['input']>;
+  /** The short description about the teacher. */
+  teacherDescription?: InputMaybe<Scalars['String']['input']>;
+  /** The specialty of the teacher. */
+  teacherSpecialty?: InputMaybe<Scalars['String']['input']>;
+};
+
+/** The role of the account. */
+export enum AccountRole {
+  Student = 'student',
+  Teacher = 'teacher'
+}
+
+/** The country info */
+export type Country = {
+  __typename?: 'Country';
+  /** The name of this country */
+  denomination: Scalars['String']['output'];
+  /** A unique id of this country */
+  id: Scalars['ID']['output'];
+  /** The iso code of this country */
+  iso: Scalars['String']['output'];
+  /** The iso3 code of this country */
+  iso3?: Maybe<Scalars['String']['output']>;
+  /** The num_code code of this country */
+  num_code?: Maybe<Scalars['String']['output']>;
+  /** The phone_code code of this country */
+  phone_code: Scalars['String']['output'];
+};
+
+/** An object type that wraps an error */
+export type Error = {
+  __typename?: 'Error';
+  /** The message of this error. */
+  message: Scalars['String']['output'];
+};
+
+/** Gender of the account. */
+export enum Gender {
+  Female = 'female',
+  Male = 'male',
+  Other = 'other'
+}
 
 export type Mutation = {
   __typename?: 'Mutation';
   /** Mutation for testing */
   isTesting?: Maybe<Scalars['Boolean']['output']>;
+  /** Updates a user account information. */
+  updateAccountInfo?: Maybe<MutationResult>;
+};
+
+
+export type MutationUpdateAccountInfoArgs = {
+  accountInfo: AccountInfoInput;
+};
+
+/** The result of a mutation. */
+export type MutationResult = {
+  __typename?: 'MutationResult';
+  /** A list of errors that occurred executing this mutation. */
+  errors: Array<Error>;
+  /** Indicates if the mutation was successful. */
+  success: Scalars['Boolean']['output'];
 };
 
 /** The open id client providers to use for auth */
@@ -37,6 +149,10 @@ export type OpenidClient = {
 
 export type Query = {
   __typename?: 'Query';
+  /** List of countries */
+  countries: Array<Country>;
+  /** The current user */
+  me: Account;
   /** List of OpenId clients */
   openIdClients: Array<OpenidClient>;
 };
