0570f94196c44d1bbe973b896e3143322849c2fb
Karim <chammout.karim@gmail.com>
2024-12-11 12:01:49 +0200

    Add `me` query field

--- src/graphql/schema/Query.ts
+++ src/graphql/schema/Query.ts
@@ -1,11 +1,20 @@
 import { GraphQLList, GraphQLNonNull, GraphQLObjectType } from 'graphql';
 
 import { ContextType } from '../../types/types';
+import { authenticated } from '../utils/auth';
+import { Account } from './types/Account';
 import OpenidClient from './types/OpenidClient';
 
 const Query = new GraphQLObjectType<any, ContextType>({
   name: 'Query',
   fields: {
+    me: {
+      type: new GraphQLNonNull(Account),
+      description: 'The current user',
+      resolve: authenticated((_, __, ctx) => {
+        return ctx.loaders.Account.loadById(ctx.user.id);
+      }),
+    },
     openIdClients: {
       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(OpenidClient))),
       description: 'List of OpenId clients',
