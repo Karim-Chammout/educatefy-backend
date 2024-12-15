f2061e5febfa9275e4027f27e87a9f2a42ed288c
Karim <chammout.karim@gmail.com>
2024-12-15 16:11:25 +0200

    Fix CORS origin config

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -5,9 +5,13 @@ const allowedDomains = [''];
 
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
+    if (!origin) {
+      return callback(null, true);
+    }
+
     // Allow all localhost ports and specific domains
     const localhostPattern = /^http:\/\/localhost(:\d+)?$/;
-    if (origin && (allowedDomains.includes(origin) || localhostPattern.test(origin))) {
+    if (allowedDomains.includes(origin) || localhostPattern.test(origin)) {
       return callback(null, true);
     }
 
