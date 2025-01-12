1816f8008ad8549bde1196a788894e4323d15012
Karim <chammout.karim@gmail.com>
2025-01-12 14:18:33 +0100

    Create `defaultContentComponentFields` GQL type

---
+++ src/graphql/schema/types/ContentComponent.ts
@@ -0,0 +1,36 @@
+import {
+  GraphQLBoolean,
+  GraphQLFieldConfigMap,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLString,
+} from 'graphql';
+
+import { ContentComponent as ContentComponentType } from '../../../types/db-generated-types';
+import { ContextType } from '../../../types/types';
+
+export const defaultContentComponentFields: GraphQLFieldConfigMap<
+  ContentComponentType,
+  ContextType
+> = {
+  denomination: {
+    type: new GraphQLNonNull(GraphQLString),
+    description: 'The denomination of the component.',
+  },
+  type: {
+    type: new GraphQLNonNull(GraphQLString),
+    description: 'The type of the component.',
+  },
+  is_required: {
+    type: new GraphQLNonNull(GraphQLBoolean),
+    description: 'A flag indicating whether the component is required to continue.',
+  },
+  is_published: {
+    type: new GraphQLNonNull(GraphQLBoolean),
+    description: 'A flag indicating whether the component is published',
+  },
+  rank: {
+    type: new GraphQLNonNull(GraphQLInt),
+    description: 'The rank of the component',
+  },
+};
