3b40c0e6b159612bc9374e49ee49e6945c2d0336
Karim <chammout.karim@gmail.com>
2025-05-01 23:49:47 +0200

    Create `PublicAccount` GQL type

---
+++ src/graphql/schema/types/PublicAccount.ts
@@ -0,0 +1,43 @@
+import { GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+
+import { Account as AccountType } from '../../../types/db-generated-types';
+import { ContextType } from '../../../types/types';
+import { getImageURL } from '../../../utils/getImageURL';
+
+export const PublicAccount = new GraphQLObjectType<AccountType, ContextType>({
+  name: 'PublicAccount',
+  description: 'The properties of a public account',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+      description: 'A unique id of this account',
+    },
+    name: {
+      type: GraphQLString,
+      description: 'The name of the account',
+    },
+    nickname: {
+      type: GraphQLString,
+      description: 'The nickname of the account',
+    },
+    first_name: {
+      type: GraphQLString,
+      description: 'The first name of the account',
+    },
+    last_name: {
+      type: GraphQLString,
+      description: 'The last name of the account',
+    },
+    avatar_url: {
+      type: GraphQLString,
+      description: 'The avatar url of this account',
+      resolve: async (parent, _, { user }) => {
+        if (!user.authenticated) return null;
+
+        if (parent.avatar_url) return getImageURL(parent.avatar_url);
+
+        return null;
+      },
+    },
+  }),
+});
