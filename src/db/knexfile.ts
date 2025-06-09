52f1a2aa6ea5b364cd2dba635a848c6e81b2c9aa
Karim <chammout.karim@gmail.com>
2025-06-09 22:47:25 +0200

    Add DATABASE_HOST to connection configuration in knexfile

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -6,6 +6,7 @@ dotenv.config({ path: '../../.env' });
 const knexConfig: Knex.Config = {
   client: 'pg',
   connection: {
+    host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
