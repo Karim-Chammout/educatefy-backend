2fe87eea546b419952d3c754a4cccfce634e9013
Karim <chammout.karim@gmail.com>
2025-12-27 02:46:53 +0100

    Create `ProgramInfo` GQL Input type

---
+++ src/graphql/schema/inputs/ProgramInfo.ts
@@ -0,0 +1,40 @@
+import { GraphQLBoolean, GraphQLInputObjectType, GraphQLNonNull, GraphQLString } from 'graphql';
+
+import ProgramLevel from '../types/enum/ProgramLevel';
+
+const ProgramInfoInput = new GraphQLInputObjectType({
+  name: 'ProgramInfoInput',
+  description: 'Input for creating a program record.',
+  fields: {
+    denomination: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The denomination of this program.',
+    },
+    slug: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The slug of this program.',
+    },
+    subtitle: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The subtitle of this program.',
+    },
+    description: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'The description of this program.',
+    },
+    level: {
+      type: new GraphQLNonNull(ProgramLevel),
+      description: 'The difficulty level of this program.',
+    },
+    image: {
+      type: GraphQLString,
+      description: 'The image of this program.',
+    },
+    is_published: {
+      type: new GraphQLNonNull(GraphQLBoolean),
+      description: 'A flag to indicate whether this program is published or not.',
+    },
+  },
+});
+
+export default ProgramInfoInput;
